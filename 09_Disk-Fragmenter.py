bf6cf969473901e5aa01942fce657cc6a10b8829
robynskyrme <robynskyrme@gmail.com>
2025-01-09 11:59:05 +0000

    Advent of Code 2024 Day 9 (Part One) -- Stuck. Checksum correct with example but not with puzzle input

--- 09_Disk-Fragmenter.py
+++ 09_Disk-Fragmenter.py
@@ -34,6 +34,9 @@ def compact(caret_block,caret_data):
     c = disk
     last_empty = table[len(table)-1]
 
+    if table[caret_block] == 0:
+        caret_block += 2
+
     if caret_block > last_empty:
         return 0,0
 
@@ -41,18 +44,6 @@ def compact(caret_block,caret_data):
         return False
 
     endblock = len(disk)-1
-
-    if len(disk[endblock]) == 0 and last_empty == 0:
-        disk.pop(endblock)
-        return 0,0
-
-    while len(disk[endblock]) == 0:
-
-        disk.pop(endblock)
-        endblock -= 1
-        if endblock % 2 == 1:
-            table[len(table)-1] -= 2
-
 
     endID = disk[endblock][len(disk[endblock])-1]
 
@@ -62,6 +53,8 @@ def compact(caret_block,caret_data):
     disk[caret_block].append(endID)
     caret_data += 1
 
+    disk[endblock] = disk[endblock][:-1]
+
     if len(disk[caret_block]) == table[caret_block] and caret_block == last_empty:
         table[len(table)-1] = 0
         return 0,0
@@ -70,11 +63,31 @@ def compact(caret_block,caret_data):
         caret_block += 2
         caret_data = 0
 
+    if len(disk[endblock]) == 0 and last_empty == 0:
+        disk.pop(endblock)
+        return 0,0
 
-    disk[endblock] = disk[endblock][:-1]
+    while len(disk[endblock]) == 0:
+        disk.pop(endblock)
+        endblock -= 1
+        if endblock % 2 == 1:
+            table[len(table)-1] -= 2
 
     return caret_block,caret_data
 
+
+def checksum():
+    checklist = []
+    sum = 0
+    for block in disk:
+        for ID in block:
+            checklist.append(ID)
+
+    for index in range(len(checklist)):
+        mult = index * checklist[index]
+        sum += mult
+
+    return sum
 
 
 if __name__ == "__main__":
@@ -83,13 +96,13 @@ if __name__ == "__main__":
     file = open("09_puzzle.aoc","r")
     data = file.read()
 
-    data = "12345"
+    data = "2333133121414131402"
 
     process(data)
     file.close()
 
-    print("TABLE",table)
-    print(" DISK",disk)
+#    print("INIT_TABLE",table)
+#    print(" INIT_DISK",disk)
 
     caret_block = 1
     caret_data = 0
@@ -99,6 +112,9 @@ if __name__ == "__main__":
         caret_block,caret_data = compact(caret_block,caret_data)
         if caret_block + caret_data == 0:
             uinp = "quit"
-    print(disk)
+
+    print("")
+#    print("COMPACTED",disk)
+    print("CHEKCSUM",checksum())
 
     print("\n/// done in " + str(time.time()-stopwatch) +" seconds ///")
